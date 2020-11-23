33fbee8b313f7ee27e68ec73857be5891b618dd5
Raphi-Sch <35998957+Raphi-Sch@users.noreply.github.com>
2020-11-23 23:31:11 +0100

    + speed() and volume()

--- src/js/player.js
+++ src/js/player.js
@@ -38,3 +38,15 @@ function play_progress(id){
   document.getElementById('progress-bar-' + id).style.width = (current / duration) * 100 + "%";
   if(current == duration) load(id);
 }
+
+function speed(id){
+  current = document.getElementById('speed-range-' + id).value;
+  document.getElementById('speed-text-' + id).innerText = current;
+  document.getElementById('player-' + id).playbackRate = current;
+}
+
+function volume(id){
+  current = document.getElementById('volume-range-' + id).value;
+  document.getElementById('volume-text-' + id).innerText = parseInt(current * 100) + "%";
+  document.getElementById('player-' + id).volume = current;
+}
