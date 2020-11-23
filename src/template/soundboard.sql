f9031eb130f7392d7980460f3d22f2000124ca01
Raphi-Sch <35998957+Raphi-Sch@users.noreply.github.com>
2020-11-23 23:33:58 +0100

    + DB template

---
+++ src/template/soundboard.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le :  lun. 23 nov. 2020 à 23:33
+-- Version du serveur :  10.3.22-MariaDB-0+deb10u1
+-- Version de PHP :  7.3.19-1~deb10u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `soundboard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `active`
+--
+
+CREATE TABLE `active` (
+  `reference` smallint(6) NOT NULL,
+  `volume` float NOT NULL DEFAULT 1,
+  `speed` float NOT NULL DEFAULT 1,
+  `audio` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `audio`
+--
+
+CREATE TABLE `audio` (
+  `reference` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `file` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `active`
+--
+ALTER TABLE `active`
+  ADD PRIMARY KEY (`reference`),
+
+--
+-- Index pour la table `audio`
+--
+ALTER TABLE `audio`
+  ADD PRIMARY KEY (`reference`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `audio`
+--
+ALTER TABLE `audio`
+  MODIFY `reference` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
