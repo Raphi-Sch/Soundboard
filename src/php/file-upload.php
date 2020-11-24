cdb88e08bec4860b115ebdf4f69f8b1512f9784a
Raphi-Sch <35998957+Raphi-Sch@users.noreply.github.com>
2020-11-24 11:05:13 +0100

    + file_upload()

---
+++ src/php/file-upload.php
@@ -0,0 +1,69 @@
+<?php
+
+function file_upload($file_field, $dest_dir, $name_prefix = "", $original_name = false, $new_name = "", $extension = ""){
+    // Return true if everything OK.
+    $MAX_FILE_SIZE = 10485760;
+    try {
+        // If this request falls under any of them, treat it invalid.
+        if (!isset($_FILES[$file_field]['error']) || is_array($_FILES[$file_field]['error'])) {
+            throw new RuntimeException('Invalid parameters');
+        }
+
+        // Check $_FILES[$file_field]['error'] value.
+        switch ($_FILES[$file_field]['error']) {
+            case UPLOAD_ERR_OK:
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                throw new RuntimeException('No file send.');
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                throw new RuntimeException('File size is larger than the server allows.');
+            default:
+                throw new RuntimeException('Unknown error');
+        }
+
+        // You should also check filesize here. 10MB = 10485760 Bytes
+        if ($_FILES[$file_field]['size'] > $MAX_FILE_SIZE) {
+            $size = $_FILES[$file_field]['size'] / $MAX_FILE_SIZE;
+            throw new RuntimeException("File size is larger than the server allows. (Limit : ".number_format($MAX_FILE_SIZE/1048576, 2)." MB, Your file : ".number_format($_FILES[$file_field]['size'] / 1048576, 2)." MB).");
+        }
+
+        // Check MIME
+        $finfo = new finfo(FILEINFO_MIME_TYPE);
+        if (false === $ext = array_search(
+            $finfo->file($_FILES[$file_field]['tmp_name']),
+            array(
+                'mp3' => 'audio/mpeg',
+            ),
+            true
+        )) {
+            throw new RuntimeException('File format not supported.');
+        }
+
+        // Name
+        if($original_name)
+            $name = pathinfo($_FILES[$file_field]['name'])['filename'];
+        else
+            if(empty($new_name))
+                $name = sha1_file($_FILES[$file_field]['tmp_name']);
+            else
+                $name = $new_name;
+
+        // Extension
+        if($extension){
+            $ext = $extension;
+        }
+
+        // Move
+        if (!move_uploaded_file($_FILES[$file_field]['tmp_name'],sprintf("$dest_dir/%s.%s",$name_prefix.$name,$ext))) {
+            throw new RuntimeException('Unable to move the file.');
+        }
+    }
+    catch (RuntimeException $e) {
+        $_SESSION['alert'] = ["error", "Error when receiving the file", $e->getMessage()];
+        return false; // Error append
+    }
+    return $name.".".$ext; // Everything OK
+}
+
+?>
