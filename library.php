747c8ee158d836e098a50cf2276392c4f812fd9d
RaphaelSch <raphael.scharff@gmail.com>
2021-11-20 13:20:15 +0100

    * library rework

--- library.php
+++ library.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 require_once('src/php/db.php');
-require_once('src/php/file-upload.php');
+require_once('src/php/functions.php');
 $db = db_connect();
 
 // POST
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if($_POST['action'] == "add" && !empty($_POST['name'])){
         $name = addslashes(trim($_POST['name']));
-        $file_name = file_upload("audio", dirname(__FILE__)."/src/audio", "", false, md5(time()));
+        $file_name = file_upload("audio", dirname(__FILE__)."/src/audio", "", false, guidv4());
         if($file_name)
             db_query_no_result($db, "INSERT INTO audio VALUES (NULL, '$name', '$file_name')");
 
@@ -40,13 +40,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 $HTML = "";
 $result = db_query_raw($db, "SELECT * FROM audio ORDER BY audio.name");
 while($row = mysqli_fetch_assoc($result)) {
+    $reference = $row["reference"];
+    $name = $row["name"];
+    $file = $row["file"];
+
     $HTML .= "
     <tr>
-        <td id='name_".$row["reference"]."'>".$row["name"]."</td>
-        <td id='file_".$row["reference"]."'><a href='src/audio/".$row["file"]."'>".$row["file"]."</a></td>
+        <td id='name_$reference'>$name</td>
+        <td><audio id='player-$ref' controls preload=none><source src='src/audio/$file' type='audio/mpeg'></audio></td>
+        <td class='center'><a href='src/audio/$file'><i class='glyphicon glyphicon-cloud-download'></i> Download</a></td>
         <td>
-          <button onClick='edit_entry(\"".$row["reference"]."\")' class='btn btn-warning' type='button'><i class='glyphicon glyphicon-pencil'></i></button>
-          <button type='button' class='btn btn-danger' onclick='del_entry(\"".$row['reference']."\")'><i class='glyphicon glyphicon-remove'></i></button>
+          <button onClick='edit_entry(\"$reference\")' class='btn btn-warning' type='button'><i class='glyphicon glyphicon-pencil'></i></button>
+          <button type='button' class='btn btn-danger' onclick='del_entry(\"$reference\")'><i class='glyphicon glyphicon-remove'></i></button>
         </td>
     </tr>";
 }
@@ -68,17 +73,12 @@ while($row = mysqli_fetch_assoc($result)) {
         <table class="table table-hover table-condensed">
             <thead>
                 <tr>
-                    <th class="col-xs-3">Name</th>
-                    <th>File</th>
-                    <th class="col-xs-2"></th>
-                </tr>
-                <tr>
-                    <form method="post" enctype='multipart/form-data'>
-                        <input type='hidden' name='action' value='add'>
-                        <td><input type="text" class="form-control" name="name" required></td>
-                        <td><input type="file" class="form-control" name="audio" accept=".mp3" required></td>
-                        <td><button type="submit" class="btn btn-success" name="action" value="add"><i class="glyphicon glyphicon-plus"></i></button></td>
-                    </form>
+                    <th class="col-xs-3">Track</th>
+                    <th>Preview</th>
+                    <th></th>
+                    <th class="col-xs-2">
+                        <button onClick='import_audio()' class='btn btn-success' type='button'><i class='glyphicon glyphicon-cloud-upload'></i></button>
+                    </th>
                 </tr>
             </thead>
             <tbody>
@@ -117,7 +117,7 @@ while($row = mysqli_fetch_assoc($result)) {
         function edit_entry(ref){
             value = document.getElementById("name_" + ref).innerText;
             Swal.fire({
-                title: 'Editing name of : "' + ref + '"',
+                title: 'Edit name',
                 type: 'info',
                 html: "<form id='Swal-form' method='post'><input type='hidden' name='action' value='edit'><input type='hidden' name='reference' value='" + ref + "'><input class='form-control' type='text' name='name' value=\"" + value + "\"></form>",
                 showCancelButton: true,
@@ -131,6 +131,28 @@ while($row = mysqli_fetch_assoc($result)) {
                     document.getElementById('Swal-form').submit();
             })
         }
+
+        function import_audio(){
+            Swal.fire({
+                title: "Import audio file",
+                html:   "<form id='swal-form' method='post' enctype='multipart/form-data'>"+
+                        "<input type='hidden' name='action' value='add'>"+
+                        "<td><input type='text' class='form-control' name='name' placeholder='Track name' required></td><br/>"+
+                        "<td colspan=2><input type='file' class='form-control' name='audio' accept='.mp3' required></td>"+
+                        "</form>",
+                showCancelButton: true,
+                showConfirmButton: confirm,
+                focusConfirm: false,
+                allowOutsideClick: false,
+                width: "25%",
+                confirmButtonText: 'Import',
+                cancelButtonText: 'Cancel'
+            }).then((result) =>{
+                if(result.value)
+                    document.getElementById('swal-form').submit();
+            });
+        }
+
     </script>
 </body>
 </html>
--- src/php/functions.php
+++ src/php/functions.php
@@ -1,5 +1,19 @@
 <?php
 
+function guidv4($data = null) {
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
+
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+
 function file_upload($file_field, $dest_dir, $name_prefix = "", $original_name = false, $new_name = "", $extension = ""){
     // Return true if everything OK.
     $MAX_FILE_SIZE = 10485760;
@@ -65,5 +79,3 @@ function file_upload($file_field, $dest_dir, $name_prefix = "", $original_name =
     }
     return $name.".".$ext; // Everything OK
 }
-
-?>
