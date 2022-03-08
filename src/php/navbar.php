586a6a2cf83f4302bedccedd7b31f2bca917b32e
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2022-03-08 11:19:46 +0100

    + sequencer page

--- src/php/navbar.php
+++ src/php/navbar.php
@@ -12,7 +12,8 @@
     <div id='navbar' class='collapse navbar-collapse'>
         <ul class="nav navbar-nav">
             <li id="index"><a href="index.php"><i class="glyphicon glyphicon-play"></i> Player</a></li>
-            <li id="library"><a href="library.php"><i class="glyphicon glyphicon-align-justify"></i> Library</a></li>
+            <li id="sequencer"><a href="sequencer.php"><i class="glyphicon glyphicon-fast-forward"></i> Sequencer</a></li>
+            <li id="library"><a href="library.php"><i class="glyphicon glyphicon-hdd"></i> Library</a></li>
             <li id="assign"><a href="assign.php"><i class="glyphicon glyphicon-wrench"></i> Assign</a></li>
         </ul>
         <ul class='nav navbar-nav navbar-right'>
