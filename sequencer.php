79793b65b9057cc46f4aa0975e63c792db2d0cb6
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2022-03-08 14:59:04 +0100

    + basic sequencer

--- sequencer.php
+++ sequencer.php
@@ -1,3 +1,94 @@
+<?php
+require_once('src/php/db.php');
+$db = db_connect();
+
+
+function build_sequence($db, $next, $order, &$data_player){
+    // Exit condition
+    if(empty($next)) return "";
+
+    // Building
+    $data = db_query($db, "SELECT sequencer.*, audio.file, audio.name FROM sequencer LEFT JOIN audio ON sequencer.audio = audio.reference WHERE sequencer.reference = '$next'");
+    $ref = $data['reference'];
+    $name = empty($data['audio']) ? "- - EMPTY - -" : $data['name'];
+    $file = $data['file'];
+    $HTML = "
+        <i class='glyphicon glyphicon-arrow-right' style='vertical-align=middle'></i>
+        <div class='card'>
+            <div class='row'>
+                <div class='col-md-12'>
+                    <div class='sample-title'>$name</div>
+                    <div class='progress'>
+                        <div id='progress-bar-$ref' class='progress-bar progress-bar-success' role='progressbar' style='width:0%'></div>
+                    </div>
+                    <button class='btn btn-info btn-player'><i class='glyphicon glyphicon-pencil'></i></button>
+                </div>
+            </div>
+            <br/>
+
+            <audio id='player-$ref' preload=none>
+                <source src='src/audio/$file' type='audio/mpeg'>
+            </audio>
+        </div>
+    ";
+
+    // Player
+    array_push($data_player, ['reference' => $ref]);
+    $order++;
+    
+    // Next element
+    $HTML .= build_sequence($db, $data['next'], $order, $data_player);
+
+    // Return
+    return $HTML;
+}
+
+$HTML_players = "";
+$data_sequence = array();
+$data_header = db_query_raw($db, "SELECT sequencer.*, audio.file, audio.name FROM sequencer LEFT JOIN audio ON sequencer.audio = audio.reference WHERE sequencer.header = 1 ORDER BY sequencer.reference");
+while($row = mysqli_fetch_assoc($data_header)) {
+    $ref = $row['reference'];
+    $name = empty($row['audio']) ? "- - EMPTY - -" : $row['name'];
+    $file = $row['file'];
+    $chain_players = array();
+
+    $HTML_players .= "
+        <div class='wrapper'>
+            <div class='card'>
+                <div class='row'>
+                    <div class='col-md-12'>
+                        <div class='sample-title'>$name</div>
+                        <div class='progress'>
+                            <div id='progress-bar-$ref' class='progress-bar progress-bar-success' role='progressbar' style='width:0%'></div>
+                        </div>
+                        <button id='btn-play-$ref' class='btn btn-success btn-player' onclick='play($ref)'><i class='glyphicon glyphicon-fast-backward'></i> <i class='glyphicon glyphicon-play'></i></button>
+                        <button id='btn-stop-$ref' class='btn btn-danger btn-player' onclick='stop($ref)'><i class='glyphicon glyphicon-stop'></i></button>
+                        <button class='btn btn-info btn-player'><i class='glyphicon glyphicon-pencil'></i></button>
+                    </div>
+                </div>
+                <br/>
+
+                <audio id='player-$ref' preload=none>
+                    <source src='src/audio/$file' type='audio/mpeg'>
+                </audio>
+            </div>
+    ";
+
+    // Header player
+    array_push($chain_players, ['reference' => $ref]);
+
+    // Every element of that chain
+    $HTML_players .= build_sequence($db, $row['next'], 1, $chain_players);
+
+    $HTML_players .= "</div><br/>";
+
+    $data_sequence = $data_sequence + [$ref => $chain_players];
+}
+
+$JSON_data_sequence = json_encode($data_sequence, JSON_FORCE_OBJECT);
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -12,12 +103,17 @@
     <!-- Main -->
     <div class="main col-md-12">
 
+        <!-- Players -->
+        <?php echo $HTML_players ?>
+        
     </div>
 
     <!-- Footer -->
     <?php include('src/html/footer.html');?>
     <script src='src/js/sequencer.js'></script>
     <script>
+        var data_sequence = JSON.parse('<?php echo $JSON_data_sequence; ?>');
+
         $(document).ready(function() {
             document.getElementById("sequencer").className="active";
         });
---
+++ src/js/sequencer.js
@@ -0,0 +1,48 @@
+var intervals = [];
+var players_state = [];
+
+function play(header_id){
+    //current_sequence = data_sequence[header_id];
+    current_sequence = [];
+    for (const [id, data] of Object.entries(data_sequence[header_id])){
+        current_sequence.push(data.reference);
+    }
+
+    current_sequence.reverse();
+
+    play_sequence(current_sequence);
+}
+
+function play_sequence(sequence){
+    console.log(sequence);
+
+    id = sequence[sequence.length - 1];
+    sequence.pop();
+
+    document.getElementById('player-' + id).currentTime = 0;
+    document.getElementById('player-' + id).play();
+    intervals[id] = setInterval(() => {
+      play_progress(id, sequence);
+    }, 10);
+}
+
+function play_progress(id, sequence){
+    current = document.getElementById('player-' + id).currentTime;
+    duration = document.getElementById('player-' + id).duration;
+    document.getElementById('progress-bar-' + id).style.width = (current / duration) * 100 + "%";
+
+    if(current == duration){
+        stop(id);
+        if(sequence.length > 0){
+            play_sequence(sequence);
+        }
+    }
+}
+
+function stop(id){
+    players_state[id] = false;
+    document.getElementById('player-' + id).pause();
+    document.getElementById('player-' + id).currentTime = 0;
+    document.getElementById('progress-bar-' + id).style.width = "0%";
+    clearInterval(intervals[id]);
+}
